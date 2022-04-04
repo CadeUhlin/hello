a7d96e3fb9fe2699bc6adb3851a259bece1070a4
Cade Uhlin <66435114+CadeUhlin@users.noreply.github.com>
2022-04-04 17:20:15 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello, Spring 2022! You're almost graduated!");
+   printf("Hello, Spring 2022! You're almost graduated!\n You're sad to see you go but we're excited to see what you become.");
    return 0;
 }
